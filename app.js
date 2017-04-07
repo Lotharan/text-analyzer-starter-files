fec43c3996b59cf8df41390b0c42c500cf680221
Lotharan <dklotharan@hotmail.com>
2017-04-06 23:23:10 -0400

    started JS work

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// your code here!
+// take user input and count  the words and return the value of the count
+function wordCount(str) {
+	return str.split('');
+}
+
+// take array output from .split and check unique words then return the number of unique words
+
+// take array and average the length of the words and return the average
+
+// stop button from submitting and refreshing the page, instead having it fire an event to take the input and provide single string
+$(function(){
+	$('button').submit(function(event){
+		event.preventDefault();
+		$('dl').removeClass('hidden');
+	});
+});
+
+
