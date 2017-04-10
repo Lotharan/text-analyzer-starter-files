00a437d8eb3805e5ce1d510fba156b54434c45d6
Lotharan <dklotharan@hotmail.com>
2017-04-10 19:12:15 -0400

    fixed hidden text

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 // take user input and count  the words and return the value of the count
 function wordCount(str) {
-	return str.split('');
+	var userInput = [];
+	return userInput.length;
 }
+$('input[name="userinput"]')
+
+.val()
 
 // take array output from .split and check unique words then return the number of unique words
+
+// store somehwere to tell how many of each ohe there are
 
 // take array and average the length of the words and return the average
 
 // stop button from submitting and refreshing the page, instead having it fire an event to take the input and provide single string
+function handleClick(){
 $(function(){
-	$('button').submit(function(event){
-		event.preventDefault();
-		$('dl').removeClass('hidden');
-	});
-});
+ 	$('button').click(function(event){
+ 		event.preventDefault();
+ 		$('dl').removeClass('hidden');
+ 	});
+ });
+}
+$(function() {
+  handleClick();
+})
 
 
